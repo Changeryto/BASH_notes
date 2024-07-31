53a29099205d8c6cf3d5ac582ceb7b337931336d
Changeryto <changeryto@hotmail.com>
2024-07-31 10:33:56 -0600

    Se explican los subtitution command.

---
+++ 01-Introduccion/5_substitutionCommand.sh
@@ -0,0 +1,9 @@
+# !usr/bin/env
+# Programa para revisar cómo ejecutar comandos dentro de un programa y almacenar en una variable para su posterior uso.
+# Autor: Rubén Téllez Gerardo -rubentellezgerardo@aol.com
+
+ubicacionActual=$(pwd)
+infoKernel=$(uname -a)
+
+echo "La ubicación actual es: $ubicacionActual"
+echo "Información del Kernel: $infoKernel"
