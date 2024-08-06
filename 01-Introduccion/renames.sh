d1a8985cab9efd358ee179f3df52a4ff1103e5ce
Changeryto <changeryto@hotmail.com>
2024-08-06 12:48:30 -0600

    Algoritmo renames.sh mejorado

--- 01-Introduccion/renames.sh
+++ 01-Introduccion/renames.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
-
+# Programa para renombrar archivos que inician en solo 1 número, añadiendo un 0 al inicio para respetar los algoritmos de sorting de nombres de archivos
+# Rubén Téllez Gerardo - @Changeryto
 for file in $(ls | grep -E -i "^[0-9]_")
 do
-    newName="0$file"
-    #echo "$file $newName"
-    git mv $file $newName
+    git mv "$file" "0$file" || mv "$file" "0$file"
 done
